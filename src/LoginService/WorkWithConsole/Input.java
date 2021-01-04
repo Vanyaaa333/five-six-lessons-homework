fde4ddc66c93ce1a459e16a9868e1947e2c9cbe2
Vanyaaa333 <76059470+Vanyaaa333@users.noreply.github.com>
2021-01-05 00:45:38 +0300

    Fixed message of not valid log or pass

--- src/LoginService/WorkWithConsole/Input.java
+++ src/LoginService/WorkWithConsole/Input.java
@@ -9,17 +9,10 @@ public class Input {
         String input = scanner.nextLine();
         if (input.isBlank()) {
             System.out.println("Введена пустая строка. Повторите ввод.");
-            getString();
-        }
-        for (int i = 0; i < input.length(); i++) {
-            if(!Character.isLetter(input.charAt(i))){
-                System.out.println("Введена цифра или пробел. Повторите ввод.");
-                input = "";
-                getString();
-            }
         }
         return input;
     }
+
     public static String getString(String message) {
         System.out.println(message);
         return getString();
